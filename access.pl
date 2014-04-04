473d6aa3cdfb140876280bb7d71d587cf15cd5c5
bigshebang <ljm3103@rit.edu>
2014-04-03 22:54:54 -0400

    updating

--- access.pl
+++ access.pl
@@ -2,7 +2,10 @@
 #analyze access to our web server
 use strict;
 
+#~constants
 my $accessLog = "/var/log/httpd/access_log";
+my $siteName = "www.yoursite.com"
+
 my $hits = `wc -l < $accessLog`;
 chomp $hits;
 my $firstLine = `head -1 $accessLog`;
@@ -14,8 +17,6 @@ if($firstLine =~ /.*\[(\d+\/\w+\/\d+):(\d.+)\s\-\d/)
         $initialDate = $1;
         $initialTime = $2;
         $initialDate =~ s/\//-/g;
-        # print "1 is '$val1'\n";
-        # print "2 is '$val2'\n";
 }
 
 my $finalDate;
@@ -26,11 +27,9 @@ if($lastLine =~ /.*\[(\d+\/\w+\/\d+):(\d.+)\s\-\d/)
         $finalDate = $1;
         $finalTime = $2;
         $finalDate =~ s/\//-/g;
-        # print "1 is '$val1'\n";
-        # print "2 is '$val2'\n";
 }
 
-print "WEBLOG REPORT FOR WWW.LUKE.COM by Luke Matarazzo\n";
+print "WEB LOG REPORT FOR $siteName by Luke Matarazzo\n";
 print "=" x 50 . "\n";
 print "Report covers period from " . $initialDate . " at " . $initialTime . " to " . $finalDate . " at " . $finalTime . "\n";
 print "There were " . $hits . " hits.\n";
